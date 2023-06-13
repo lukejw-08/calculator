6e00ce296405f944a227f4cf9451b07ef5ecd6b9
lukejw-08 <lukewester01@gmail.com>
2023-06-13 16:15:50 +0100

    calc push

--- calc/MainWindow.xaml.cs
+++ calc/MainWindow.xaml.cs
@@ -24,5 +24,11 @@ namespace calc
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+            
+        }
     }
 }
